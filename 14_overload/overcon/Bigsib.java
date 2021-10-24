768c589c9e32aa2b1eb3f92a56fe10805d517238
Oscar Breen <90660886+Ozb888@users.noreply.github.com>
2021-10-24 18:48:42 -0400

    Create Bigsib.java

---
+++ 14_overload/overcon/Bigsib.java
@@ -0,0 +1,21 @@
+//JoscarA -- RedHead Phoenix Green Lantern
+//APCS
+//HW14 -- Customize Your Creation -- differing constructors for overload
+//2021-10-08
+public class BigSib{
+String mmsg;
+public BigSib(){
+  System.out.print("whats up ");
+  }
+public BigSib(String s){
+  System.out.print("My bro ");
+  System.out.println(s);
+
+  }
+  public static String greet(String mmsg) {
+      return mmsg;
+    }
+  }
+
+//DISCOVERIES-- to overload consturcts give them differing number of args
+//UNRESOLVED QUESTIONS-- still not sure why this is better than a Method
