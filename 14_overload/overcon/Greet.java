3af2b9db196b930bee5a638d1a99b3f5ef7eec07
Oscar Breen <90660886+Ozb888@users.noreply.github.com>
2021-10-24 18:49:27 -0400

    Create Greet.java

---
+++ 14_overload/overcon/Greet.java
@@ -0,0 +1,11 @@
+
+public class Greet{
+    public static void main( String[] args ) {
+BigSib mybrogreetF = new BigSib("Flim");
+        mybrogreetF.greet("Flim");
+BigSib mybrogreetFa = new BigSib("Flam");
+        mybrogreetFa.greet("Flam");
+BigSib mybrogreetK = new BigSib("Kazaam");
+      mybrogreetK.greet("Kazaam");
+    }
+}
