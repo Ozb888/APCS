ee6fcf935734306492fc04cc361a30bfb1807a68
Oscar Breen <90660886+Ozb888@users.noreply.github.com>
2021-10-24 18:57:35 -0400

    Create Greet.java

---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,13 @@
+//Frog Hats -- RedHead FrogHat Ajax
+//APCS
+//HW09 -- What are bigsibs good for? -- Methods through different files-- maybe
+//2021-10-04
+public class Greet {
+    public static void main( String[] args ) {
+        BigSib.greet("Flim");
+        BigSib.greet("Flam");
+        BigSib.greet("Kazaam");
+    }
+}
+//DISCOVERIES
+//UNRESOLVED QUESTIONS
