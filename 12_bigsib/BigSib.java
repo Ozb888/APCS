67dae0b34edab91051e6c2a20211789746d7f9f6
Oscar Breen <90660886+Ozb888@users.noreply.github.com>
2021-10-24 18:54:09 -0400

    Create BigSib.java

---
+++ 12_bigsib/BigSib.java
@@ -0,0 +1,15 @@
+//Frog Hats -- RedHead FrogHat Ajax
+//APCS
+//HW09-- On Elder Individuality and the Elimination of Radio Fuzz -- Methods through different files and "Instances"
+//2021-10-06
+//DISCO-- not sure
+//QCC-- why do we make it equal to greeting then print greeting instead of a direct print?
+public class BigSib{
+    public static void greet(String s) {
+      System.out.println("Whats Up");
+      System.out.println(setHelloMsg());
+    }
+public static String setHelloMsg(String s) {
+  System.out.println(s);
+    }
+}
