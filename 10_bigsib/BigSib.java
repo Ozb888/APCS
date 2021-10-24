6dd488bca04808c4df415638dcd456e981ecf58a
Oscar Breen <90660886+Ozb888@users.noreply.github.com>
2021-10-24 18:55:26 -0400

    Create BigSib.java

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,16 @@
+//Frog Hats -- RedHead FrogHat Ajax
+//APCS
+//HW09 -- What are bigsibs good for? -- Methods through different files-- maybe
+//2021-10-04
+public class BigSib{
+    public static void greet(String s) {
+System.out.println("I speak cheese");
+System.out.println(s);
+System.out.println("Do you speak cheese");
+System.out.println(s);
+System.out.println("No hablo queso");
+    }
+}
+
+//DISCOVERIES
+//UNRESOLVED QUESTIONS
