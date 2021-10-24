57ca876a3f7055ba519916de4b5babd0f9c4f871
Oscar Breen <90660886+Ozb888@users.noreply.github.com>
2021-10-24 18:59:43 -0400

    Create Greet1.java

---
+++ 07_helloclass/Greet1.java
@@ -0,0 +1,9 @@
+//Oscar Breen
+//APCS
+//HW07 -- first greet use, test
+//2021-09-30.
+public class Greet1{
+  public static void main(String[]args){
+    System.out.println("No hablo queso");
+  }
+}
