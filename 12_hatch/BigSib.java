0269910cfdd32f2578e6f180cddff9dd95f2227d
Oscar Breen <90660886+Ozb888@users.noreply.github.com>
2021-10-24 18:50:21 -0400

    Create BigSib.java

---
+++ 12_hatch/BigSib.java
@@ -0,0 +1,16 @@
+//Joscar -- RedHead, Phoenix
+//APCS
+//HW13-- Where do BigSibs Come From?-- Using a Constructor
+//2021-10-07
+//DISCO-- a constructor can replace unnesessary code
+//QCC-- is this not just the same as a method?
+public class BigSib{
+String mmsg = new String();
+    public BigSib() {
+System.out.print("whats up ");
+    }
+public static String greet(String mmsg) {
+return mmsg;
+
+  }
+}
