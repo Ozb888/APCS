3be698c6021e2e2115f66d19125f33043b2bef76
Oscar Breen <90660886+Ozb888@users.noreply.github.com>
2021-10-24 18:56:02 -0400

    Create Greet.java

---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,11 @@
+//Frog Hats -- RedHead FrogHat Ajax
+//APCS
+//HW09-- Refracter Big Sib One -- Methods through different files again
+//2021-10-05
+public class Greet {
+    public static void main( String[] args ){
+        System.out.println(BigSib.greet("Crosby") );
+        System.out.println(BigSib.greet("Stills") );
+        System.out.println(BigSib.greet("Nash") );
+    }
+}
