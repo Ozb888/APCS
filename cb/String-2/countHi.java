9a28c6eb4f07c662b193e3e0356b65798e8db5b7
Oscar Breen <90660886+Ozb888@users.noreply.github.com>
2021-11-08 21:09:27 -0500

    Hw

---
+++ cb/String-2/countHi.java
@@ -0,0 +1,17 @@
+public class countHi{
+
+public static int countHi(String str) {
+  int count = 0;
+  for( int i = 0 ; i < str.length()-1 ; i++){
+  if ( str.substring(i , i+2).equals("hi"))
+  count = count + 1;
+  }
+  return count;
+}
+
+public static void main (String[] args) {
+System.out.println(countHi("abc hi ho"));
+System.out.println(countHi("ABChi hi"));
+System.out.println(countHi("hihi"));
+}
+}
