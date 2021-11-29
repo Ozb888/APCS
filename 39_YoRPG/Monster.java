5237f40249a61c6935c4e89e236fa5a6bff321db
Oscar Breen <90660886+Ozb888@users.noreply.github.com>
2021-11-29 10:28:24 -0500

    Update Monster.java

--- 39_YoRPG/Monster.java
+++ 39_YoRPG/Monster.java
@@ -2,23 +2,29 @@ public class Monster {
 	public int health = 20;
 	public boolean alive = true;
 	public int damage;
-
-	// we will probably not need this
 	public Monster() {
 	}
-	public void damage(int hurtPoints) {
-                health -= hurtPoints;
+	
+
+
+          
+        public void damage(int hurtPoints) {
+                health = (health - hurtPoints);
                 if (health <= 0)
                         alive = false;
         }
-	public int attack(Protagonist a) {
-		damage = (int) (Math.random() * 6); // random int in random [0, 5)
-		a.health -= damage;
-		if (a.health < 0)
-			a.alive = false;
-		return damage;
-	}
+
+
 	public boolean isAlive() {
 		return alive;
 	}
+
+        public int attack(Protagonist he) {
+		damage = (int) (Math.random() * 6); // random int in random [0, 5)
+		he.health -= damage;
+		if (he.health < 0)
+		      he.alive = false;
+		return damage;
+	}
+
 }
