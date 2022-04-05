202aeb7a9977084b1580a2ecfaa8cbadf3b44be0
Oscar Breen <obreen30@stuy.edu>
2022-04-04 21:28:26 -0400

    update not done

---
+++ 88_rqueue/Queue.java
@@ -0,0 +1,27 @@
+/***
+ * interface Queue
+ * Includes only methods listed in AP subset of Java
+ * (Local version, to override built-in Java implementation.)
+ **/
+
+public interface Queue<SWASHBUCKLE>
+{
+  //~~~~~~~~~~~~~~~~~~begin AP subset~~~~~~~~~~~~~~~~~~
+  //means of removing an element from collection:
+  //Dequeues and returns the first element of the queue.
+  public SWASHBUCKLE dequeue();
+
+  //means of adding an element to collection:
+  //Enqueue an element onto the back of this queue.
+  public void enqueue( SWASHBUCKLE x );
+
+  //Returns true if this queue is empty, otherwise returns false.
+  public boolean isEmpty();
+
+  //Returns the first element of the queue without dequeuing it.
+  public SWASHBUCKLE peekFront();
+  //~~~~~~~~~~~~~~~~~~~end AP subset~~~~~~~~~~~~~~~~~~~
+
+  public void sample();
+
+}//end interface Queue
