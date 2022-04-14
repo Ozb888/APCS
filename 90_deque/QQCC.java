696850a4a4e2d4f9eed671223686e764296599f1
Oscar Breen <obreen30@stuy.edu>
2022-04-13 21:07:50 -0400

    hw

--- 90_deque/QQCC.java
+++ 90_deque/QQCC.java
@@ -1,115 +1,75 @@
-import java.util.ArrayList;
+/*
+Team Pom Pom: Max Schneider, Perry Huang, Oscar Breen
+APCS
+HW91 -- Deque the Halls
+2022-04-13
+time spent: 1 hr
+*/
+import java.util.Iterator;
+import java.util.Collection;
+import java.util.LinkedList;
 public class QQCC<T> implements Deque<T> {
-    private ArrayList<T> _data;
-    private int _first;
-    private int _last;
-    private int _size;
+    private LinkedList<T> _data;
+    public QQCC () {
 
-    public QQCC (int size) {
+        _data = new LinkedList<T>();
 
-        _data = new ArrayList<T>(size);
-        _first = 0;
-        _last = 0;
-        _size = size;
-        
     }
 
     public void addFirst(T newVal) {
-        ArrayList<T> _temp = new ArrayList<T>(_size);
-        _temp.add(newVal);
-        for (int i = _first; i < _last; i++) {
-            _temp.add(_data.get(i));
-        }
-        _data = _temp;
-        _last++;
+        _data.addFirst(newVal);
     }
     public void addLast(T newVal) {
-        _data.add(newVal);
-        _last++;
+        _data.addLast(newVal);
     }
 
     public T pollFirst() {
-        T retVal = _data.get(_first);
-        _first += 1;
-        return retVal;
+        return _data.pollFirst();
     }
 
     public T pollLast() {
-        T retVal = _data.get(_last-1);
-        _last -= 1;
-        return retVal;
+        return _data.pollLast();
     }
 
     public T peekFirst() {
-        return _data.get(_first);
+        return _data.peekFirst();
     }
 
     public T peekLast() {
-        return _data.get(_last-1);
-
+        return _data.peekLast();
     }
 
     public boolean offerFirst(T newVal) {
-        if (_last != _data.size()) {
-            addFirst(newVal);
-            return true;
-        }
-        return false;
+        return _data.offerFirst(newVal);
     }
 
     public boolean offerLast(T newVal) {
-        if (_last != _data.size()) {
-            addLast(newVal);
-            return true;
-        }
-        return false;
+        return _data.offerLast(newVal);
     }
 
+    public boolean contains(T findVal) {
+      return _data.contains(findVal);
+    }
+
+    public Iterator<T> iterator() {
+        return _data.iterator();
+    }
+    public boolean addAll (Collection<T> c){
+        return _data.addAll(c);
+    }
+    public Iterator<T> descendingIterator(){
+        return _data.descendingIterator();
+    }
+
+    public boolean remove(T o) {
+        return _data.remove(o);
+    }   
+    public boolean removeLastOccurrence(T o) {
+        return _data.removeLastOccurrence(o);
+    }
+    
     public String toString() {
-        ArrayList<T> _temp = new ArrayList<T>();
-        _temp = _data; 
-        String retVal = "";
-        for (int i = 0; i < _last; i++) {
-            retVal += _temp.get(i);
-        }
-        return retVal;
-    }
-
-    public static void main(String[] args) {
-
-        QQCC<Integer> perry = new QQCC<Integer>(6);
-        perry.addFirst(1);
-        perry.addFirst(2);
-        perry.addFirst(3);
-        perry.addFirst(4);
-        perry.addFirst(5);
-        perry.addFirst(6);
-        perry.addFirst(7);
-
-        System.out.println(perry); //7654321
-
-
-	perry.addLast(8);
-        System.out.println(perry); //76543218
-        System.out.println(perry.peekLast()); //8
-        System.out.println(perry.peekFirst()); //7
-
-	perry.pollFirst();
-	perry.pollLast();
-        System.out.println(perry.peekLast()); //8
-        System.out.println(perry.peekFirst()); //7
-
-	perry.pollFirst();
-	perry.pollFirst();
-	perry.pollFirst();
-	perry.pollFirst();
-	perry.pollFirst();
-	perry.pollFirst();
-
-	System.out.println(perry.peekLast()); //8
-        System.out.println(perry.peekFirst()); //7
-
-
+        return _data.toString();
     }
 
 }
--- 90_deque/README.md
+++ 90_deque/README.md
@@ -1,24 +1,25 @@
-Oscar Breen, Perry Huang, Max Schneider
-APCS
-HW90 --  Swabbing the Deque/using Deque Interface/ ArrayList deque both way queue
-2022-04-- 12
-time spent: 1.2
+Team Pom Pom: Max Schneider, Perry Huang, Oscar Breen <br>
+APCS                                                  <br>
+HW91 -- Deque the Halls                               <br>
+2022-04-13                                            <br>
+time spent: 1 hr                                      <br>
 
-DISCO: To implement with ArrayList functionalities means having a O(n) runtime for addFirst
+To Do:                                      <br>
+All done!                                   <br>
 
-QCC: Can a LLDeque do this in all O(1)?
+LinkedList - Mostly EZ                      <br>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+public void addFirst(T newVal); EZ          <br>
+public void addLast(T newVal); EZ           <br>
+public T pollFirst(); EZ                    <br>
+public T pollLast(); EZ                     <br>
+public T peekFirst(); EZ                    <br>
+public T peekLast(); EZ                     <br>
+public boolean contains(T findVal); EZ      <br>
+public Iterator<T> iterator(); Medium       <br>
+public boolean addAll (Collection<T> c); EZ <br>
+public Iterator<T> descendingIterator(); EZ <br>
+public boolean offerFirst(T newVal); EZ     <br>
+public boolean offerLast(T newVal); EZ      <br>
+public boolean remove(T o);                 <br>
+public boolean removeLastOccurence(T o);    <br>
